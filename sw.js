4fa452ff7edd2995f4ff7998ffe071753c9add2b
narcolepticdoc <42015510+narcolepticdoc@users.noreply.github.com>
2026-03-04 10:23:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // INCREMENT THIS every time you deploy a new version of the app.
 // Changing this string is what tells browsers to throw away the old cache
 // and fetch everything fresh. Matches APP_VERSION in index.html.
-const VERSION = '0.3.9';
+const VERSION = '0.4.2';
 const CACHE = `reddivault-${VERSION}`;
 
 const ASSETS = [
